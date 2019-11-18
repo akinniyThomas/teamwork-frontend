3b914dee450fde6b7a2b8320311d296e35c6a48e
akinniyThomas <akinniyithomas@yahoo.com>
2019-11-18 03:58:35 +0100

    can delete gif...no css yet

--- frontend/src/Gif.js
+++ frontend/src/Gif.js
@@ -5,9 +5,24 @@ class Gif extends React.Component {
     //     super(props);
     // }
 
+    deleteGif = async (e) => {
+        const token = this.props.user.token;
+        const api = `http://localhost:8000/api/v1/gifs/${this.props.feed.id}`;
+        const response = await fetch(api, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            }
+        });
+        const jsonResponse = await response.json();
+        if (jsonResponse.status === 'success') {
+            alert('Gif successfully deleted!');
+        }
+    }
+
     render() {
         let delet; let authorName;
-        // let edit;
         const category = 'category here';
         const dateAndTime = '23-12-2019 3:22am';
         if (this.props.user.userId === this.props.feed.authorid){
@@ -20,7 +35,7 @@ class Gif extends React.Component {
                 <span>{authorName}</span>
                 <img src = {this.props.feed.feed} alt={this.props.feed.title}></img>
                 <span onClick={(e) => this.props.change(4)}>comments</span>
-                <span>{delet}</span>
+                <span onClick = {this.deleteGif}>{delet}</span>
                 {/* <span>{edit}</span> */}
                 <span>{category}</span>
                 <span>{dateAndTime}</span>
