9fb9a1c1d245069a9f0601c2dc61cb1d8c26a277
akinniyThomas <akinniyithomas@yahoo.com>
2019-11-26 05:35:30 +0100

    Updates

---
+++ precache-manifest.e84ef18ed41945900b1871a3bca6fac9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79d24672132a8eda51c62a65b25506f3",
+    "url": "/frontend-teamwork/index.html"
+  },
+  {
+    "revision": "1ea1363da84c6252235e",
+    "url": "/frontend-teamwork/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "eaaaabac0a5451fb6c61",
+    "url": "/frontend-teamwork/static/css/main.bf838d41.chunk.css"
+  },
+  {
+    "revision": "1ea1363da84c6252235e",
+    "url": "/frontend-teamwork/static/js/2.b10076b6.chunk.js"
+  },
+  {
+    "revision": "eaaaabac0a5451fb6c61",
+    "url": "/frontend-teamwork/static/js/main.543c9478.chunk.js"
+  },
+  {
+    "revision": "ee9e1c5577b825ec36cb",
+    "url": "/frontend-teamwork/static/js/runtime-main.a6ca496b.js"
+  }
+]);
